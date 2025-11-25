091a7e01b8da9ff3211d2ae1271863d5be669a74
Tetiana <jany.starling@gmail.com>
2025-11-25 13:11:06 +0200

    Created page Dashboard and structure for the components

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Header from "../Header/Header.tsx";
+import Content from "../Content/Content.tsx";
 
 function App() {
 
   return (
     <>
+        <Header />
+        <Content />
     </>
   )
 }
---
+++ client/src/components/Content/Content.tsx
@@ -0,0 +1,11 @@
+import DashboardPage from "../../pages/DashboardPage/DashboardPage.tsx";
+
+function Content() {
+    return (
+        <div>
+            <DashboardPage />
+        </div>
+    );
+}
+
+export default Content;
